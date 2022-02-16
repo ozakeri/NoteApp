980f035e4df5abdf7c8430bed39998ec5755370c
gap <omidzakeri22@gmail,com>
2022-02-16 11:46:10 +0330

    commit by omid

--- app/src/main/java/com/example/mynotapplication/SingleNotFragment.kt
+++ app/src/main/java/com/example/mynotapplication/SingleNotFragment.kt
@@ -14,6 +14,8 @@ class SingleNotFragment : Fragment() {
 
     lateinit var fragmentSingleNoteBinding: FragmentSingleNoteBinding
 
+    private var colorValueClicked = "#000000"
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,9 +24,42 @@ class SingleNotFragment : Fragment() {
 
         fragmentSingleNoteBinding = FragmentSingleNoteBinding.inflate(inflater, container, false)
 
-        fragmentSingleNoteBinding.buttonSecond
+        fragmentSingleNoteBinding.singleNoteFragment = this
+
         return fragmentSingleNoteBinding.root
 
+    }
+
+    fun onAddNoteClick(view: View) {
+
+    }
+
+    fun onColorViewClick(view: View) {
+        uncheckAllImage()
+
+        fragmentSingleNoteBinding.apply {
+            when(view.id){
+                this.check1.id -> colorValueClicked = "#64C8FD"
+                this.check2.id -> colorValueClicked = "#8069FF"
+                this.check3.id -> colorValueClicked = "#FFCC36"
+                this.check4.id -> colorValueClicked = "#D77FFD"
+                this.check5.id -> colorValueClicked = "#FF419A"
+                this.check6.id -> colorValueClicked = "#7FFB76"
+            }
+        }
+
+        view.visibility = View.VISIBLE
+    }
+
+    private fun uncheckAllImage() {
+        fragmentSingleNoteBinding.apply {
+            this.check1.visibility = View.GONE
+            this.check2.visibility = View.GONE
+            this.check3.visibility = View.GONE
+            this.check4.visibility = View.GONE
+            this.check5.visibility = View.GONE
+            this.check6.visibility = View.GONE
+        }
     }
 
 }
